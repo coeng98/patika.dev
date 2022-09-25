624c59f85e92934e3e2b93c7b8412c58a6a36db3
seydagulgun <46301727+coeng98@users.noreply.github.com>
2022-09-25 23:33:59 +0300

    Add files via upload

---
+++ Pratikler/switch-case/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace switch_case
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            //Expression
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak ayındayısınz.");
+                    break;
+                
+                case 2:
+                    Console.WriteLine("Şubat ayındayısınz.");
+                    break;
+                
+                case 4:
+                    Console.WriteLine("Nisan ayındayısınz.");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Mart ayındayısınz.");
+                    break;
+                
+                default:
+                    Console.WriteLine("Yanlış veri girişi.");
+                break;
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış ayındasınız.");
+                    break;
+
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar ayındasınız.");
+                    break;
+
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz ayındasınız.");
+                    break;
+
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar ayındaısnız.");
+                    break;
+                
+                default:
+                break;
+            }
+        }
+    }
+}
